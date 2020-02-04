b65d51f000ed6abcfcca837daeff7642bf4aa7d1
tcrurav <tiburcio.cruz@gmail.com>
2020-02-04 16:03:07 +0000

    second commit

--- js/file.js
+++ js/file.js
@@ -3,6 +3,28 @@ window.onload = initialize;
 function initialize(){
     document.getElementById("form-file-upload")
         .addEventListener("submit", uploadFile);
+
+    //downloadFile("Brasov.JPG");
+
+    showBicycles();
+}
+
+function showBicycles(){
+    var refBicycles = firebase.database().ref().child("BicycleStore/bicycles");
+    var newColumns = "";
+    refBicycles.on("value", function(snap){
+        var data = snap.val();
+        for(var key in data){
+            newColumns += 
+                '<div class="col-sm-4">' +
+                    '<img class="img-fluid" src="' + data[key].image_url + 
+                        '" alt="image"/>' +
+                '</div>';
+        }
+        document.getElementById("new-columns").innerHTML = newColumns;
+    });
+
+    
 }
 
 function uploadFile(event){
@@ -16,5 +38,36 @@ function uploadFile(event){
     var ref = firebase.storage().ref().child("algo.jpg");
     ref.put(file).then(function(snapshot) {
       console.log('Uploaded a blob or file!');
+      
+      var refDatabase = firebase.database().ref().child("BicycleStore/bicycles");
+      refDatabase.push({
+        model: "uno cualquiera",
+        color: "uno cualquiera",
+        stock: 20,
+        image_url: 
+      });
+
     });
+}
+
+function downloadFile(imageName){
+    var storageRef = firebase.storage().ref();
+    storageRef.child(imageName).getDownloadURL().then(function(url) {
+        // `url` is the download URL for 'images/stars.jpg'
+      
+        // This can be downloaded directly:
+        // var xhr = new XMLHttpRequest();
+        // xhr.responseType = 'blob';
+        // xhr.onload = function(event) {
+        //   var blob = xhr.response;
+        // };
+        // xhr.open('GET', url);
+        // xhr.send();
+      
+        // Or inserted into an <img> element:
+        var img = document.getElementById('downloaded-img');
+        img.src = url;
+      }).catch(function(error) {
+        // Handle any errors
+      });
 }
